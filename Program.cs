5f1ff1d5cd4825bce7cf3a3ca8674c1f737a9401
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-17 03:11:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CA01ThreadVsTasks {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var th = new Thread(() => Display("Metigator using thread !!!"));
+                th.Start ();
+            th.Join ();
+            Task.Run(() => Display("Metigator using task !!!")).Wait();
+           // Display("Dummy Text");
+            Console.ReadKey();
+        }
+        static void Display(string message)
+        {
+            ShowThreadInfo(Thread.CurrentThread);
+            Console.WriteLine(message);
+        }
+
+        private static void ShowThreadInfo(Thread th)
+        {
+            Console.WriteLine($"TID: {th.ManagedThreadId},Pooled: {th.IsThreadPoolThread},Background {th.IsBackground}");
+        }
+    } 
+}
